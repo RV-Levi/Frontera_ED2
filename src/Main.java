2b324999672ae04105ab963fd13e8e43ede51b74
Luis Arteaga <72145368+RV-Levi@users.noreply.github.com>
2022-03-23 14:49:42 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,7 @@ public class Main {
     public static void main(String[] arg) {
         ArbolBinario abo = new ArbolBinario();
         int var1, var2, iter;
-        Double valor;
-        Double inser;
+        Double valor, inser;
         Scanner texto = new Scanner(System.in);
 
         System.out.println("Cómo desea ingresar datos?\n"
